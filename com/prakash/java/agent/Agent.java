2da641ec44cba3713a049d2e61cc239a11ccfd96
Prakash <Prakash@Prakash.local>
2016-08-22 09:03:08 +0530

    Changing indentation to spaces

--- com/prakash/java/agent/Agent.java
+++ com/prakash/java/agent/Agent.java
@@ -12,33 +12,33 @@ import java.lang.management.MemoryType;
 // import com.ibm.lang.management.*;
 
 class Agent {
-	private static boolean init;
+    private static boolean init;
 
-	public static void premain(String args, Instrumentation instrumentation) {
-		System.out.println("Agent Pre-Main, Args: " + args);
+    public static void premain(String args, Instrumentation instrumentation) {
+        System.out.println("Agent Pre-Main, Args: " + args);
 
-		MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
-		NotificationEmitter emitter = (NotificationEmitter) mbean;
-		MyListener listener = new MyListener();
-		emitter.addNotificationListener(listener, null, null);
-		init();
-	}
+        MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
+        NotificationEmitter emitter = (NotificationEmitter) mbean;
+        MyListener listener = new MyListener();
+        emitter.addNotificationListener(listener, null, null);
+        init();
+    }
 
-	public synchronized static void init() {		
-		if(init) 
-			return;
+    public synchronized static void init() {        
+        if(init) 
+            return;
 
-		for(MemoryPoolMXBean pool : ManagementFactory.getMemoryPoolMXBeans()) {
-			if (pool.getType() == MemoryType.HEAP && pool.isUsageThresholdSupported()) {
-				System.out.println("Pool name: " + pool.getName() + ", Pool type: " + pool.getType());
-				
-				long maxMemory = pool.getUsage().getMax();
-				long warningThreshold = (long) (maxMemory * 0.3);
-				System.out.println("MaxMemory: " + maxMemory + ", WarningThreshold: " + warningThreshold);
+        for(MemoryPoolMXBean pool : ManagementFactory.getMemoryPoolMXBeans()) {
+            if (pool.getType() == MemoryType.HEAP && pool.isUsageThresholdSupported()) {
+                System.out.println("Pool name: " + pool.getName() + ", Pool type: " + pool.getType());
+                
+                long maxMemory = pool.getUsage().getMax();
+                long warningThreshold = (long) (maxMemory * 0.3);
+                System.out.println("MaxMemory: " + maxMemory + ", WarningThreshold: " + warningThreshold);
 
-				pool.setUsageThreshold(warningThreshold);
-			}
-		}
-		init = true;
-	}
+                pool.setUsageThreshold(warningThreshold);
+            }
+        }
+        init = true;
+    }
 }
